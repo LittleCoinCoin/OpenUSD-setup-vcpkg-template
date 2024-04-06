fab7868b37a960fe7ffab8588917db8be906c118
Jacopin Eliott <eliott.jacopin@laposte.net>
2024-04-06 23:05:05 +0900

    [Update] Correct USD plugin addition to the project
    Major:
    - The project was compiling just fine in the previous version commits but would crash at run time because the exe couldn't find the plugins.
    - This commit fixes this issue by writing the CMake commands to copy over the folders corresponding to the required plugins of the application.
    - Added test example functions in main.cpp.
    - The added plugins at this stage are the minimal set of plugins considering the USD functionalities used in the test functions.

--- USDTest/headers/main.h
+++ USDTest/headers/main.h
@@ -1,1 +1,9 @@
 ﻿#pragma once
+
+// for the minimal example involving the creation of a USD stage in TestFunction_StageCreation
+#include "pxr/usd/usd/stage.h" 
+
+// For TestFunction_PixarTutorial_HelloWorld
+#include "pxr/usd/usdGeom/xform.h" 
+#include "pxr/usd/usdGeom/sphere.h"
+#include "pxr/usd/sdf/path.h"
--- USDTest/src/main.cpp
+++ USDTest/src/main.cpp
@@ -1,11 +1,56 @@
 ﻿
+#include <iostream>
+#include <string>
 #include "main.h"
 
-#include "pxr/usd/usd/stage.h"
+/*!
+@brief The most basic test function that creates a new USD stage.
+@details I expect this function to run right away without any issues.
+		 In fact, it requires at least the plugins "ar", "sdf", and "usd"
+		 at runtime.
+*/
+void TestFunction_StageCreation()
+{
+	// Create a new stage
+	pxr::UsdStageRefPtr stage = pxr::UsdStage::CreateInMemory();
+}
+
+/*!
+@brief Function reproducing the first example of the Pixar USD tutorial.
+@see https://openusd.org/release/tut_helloworld.html 
+	 https://openusd.org/release/tut_helloworld_redux.html
+*/
+void TestFunction_PixarTutorial_HelloWorld()
+{
+	/* Python code from the tutorial
+		
+		from pxr import Usd, UsdGeom
+		stage = Usd.Stage.CreateNew('HelloWorld.usda')
+		xformPrim = UsdGeom.Xform.Define(stage, '/hello')
+		spherePrim = UsdGeom.Sphere.Define(stage, '/hello/world')
+		stage.GetRootLayer().Save()
+	*/
+
+	//Write the C++ equivalent of the above python code
+	pxr::UsdStageRefPtr stage = pxr::UsdStage::CreateNew("HelloWorld.usda");
+	pxr::UsdGeomXform xformPrim = pxr::UsdGeomXform::Define(stage, pxr::SdfPath("/hello"));
+	pxr::UsdGeomSphere spherePrim = pxr::UsdGeomSphere::Define(stage, pxr::SdfPath("/hello/world"));
+
+	std::string fileResult;
+	stage->GetRootLayer()->ExportToString(&fileResult);
+	std::cout << fileResult << std::endl;
+
+	stage->GetRootLayer()->Save(); // Save the stage at the same location as the application.
+}
 
 int main()
 {
-	pxr::UsdStageRefPtr stage = pxr::UsdStage::CreateInMemory();
+
+	TestFunction_StageCreation();
+
+	TestFunction_PixarTutorial_HelloWorld();
+
+	std::cout << "End of main." << std::endl;
 
 	return 0;
 }
