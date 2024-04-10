4956cf2f4d454363116e4cb408d33620bf027b68
Jacopin Eliott <eliott.jacopin@laposte.net>
2024-04-10 16:36:13 +0900

    [Add] Tutorial on traversing a stage
    Major:
    - Reproduced https://openusd.org/release/tut_traversing_stage.html

--- OpenUSD-setup-vcpkg/headers/main.h
+++ OpenUSD-setup-vcpkg/headers/main.h
@@ -11,5 +11,8 @@
 // For TestFunction_PixarTutorial_InspectAndAuthorProperties
 #include "pxr/usd/usd/tokens.h"
 
-// For TestFunction_ReferencingLayers
+// For TestFunction_PixarTutorial_ReferencingLayers
 #include "pxr/usd/usdGeom/xformCommonAPI.h"
+
+// For TestFunction_PixarTutorial_StageTraversal
+#include "pxr/usd/usd/primRange.h"
--- OpenUSD-setup-vcpkg/src/main.cpp
+++ OpenUSD-setup-vcpkg/src/main.cpp
@@ -1,4 +1,4 @@
-
+﻿
 #include <iostream>
 #include <string>
 #include "main.h"
@@ -258,8 +258,130 @@ void TestFunction_PixarTutorial_ReferencingLayers()
 	refStage->GetRootLayer()->ExportToString(&fileResult);
 	std::cout << "Content of file RefExample.usda after setting the display color of /refSphere2/world to red:\n" << fileResult << std::endl;
 
+	stage->GetRootLayer()->Save();
 	refStage->GetRootLayer()->Save();
 }
+
+/*!
+@brief Function reproducing the fifth item of the Pixar USD tutorial
+@see https://openusd.org/release/tut_traversing_stage.html
+*/
+void TestFunction_StageTraversal()
+{
+	// This step of the tutorial originally uses usdview and the python interpreter.
+	// I will adapt the demonstrated commands to C++ code.
+
+	// -- Step 1
+	std::cout << "---- Step 1 ----" << std::endl;
+
+	/*! The Python commands in the interpreter
+		[x for x in usdviewApi.stage.Traverse()]
+		# Output: [Usd.Prim(</refSphere>), Usd.Prim(</refSphere/world>), Usd.Prim(</refSphere2>), Usd.Prim(</refSphere2/world>)]
+	*/
+
+	// Write the C++ equivalent of the python code above
+	pxr::UsdStageRefPtr refStage = pxr::UsdStage::Open("RefExample.usda");
+	pxr::UsdPrimRange allPrims = refStage->Traverse();
+	std::cout << "All prims in the stage of RefExample.usda:" << std::endl;
+	for (pxr::UsdPrim prim : allPrims)
+	{
+		std::cout << prim.GetPath() << std::endl;
+	}
+
+	// -- Step 2
+	std::cout << "---- Step 2 ----" << std::endl;
+
+	/*! The Python commands in the interpreter
+		[x for x in usdviewApi.stage.Traverse() if UsdGeom.Sphere(x)]
+		# Output: [Usd.Prim(</refSphere/world>), Usd.Prim(</refSphere2/world>)]
+	*/
+
+	// Write the C++ equivalent of the python code above
+	std::vector<pxr::UsdGeomSphere> allSpheres;
+	for (auto it = allPrims.begin(); it != allPrims.end(); ++it)
+	{
+		if ((*it).IsA<pxr::UsdGeomSphere>())
+		{
+			allSpheres.push_back(pxr::UsdGeomSphere(*it));
+		}
+	}
+
+	std::cout << "All prims in the stage of RefExample.usda that are UsdGeomSpheres:\n" << std::endl;
+	for (pxr::UsdGeomSphere sphere : allSpheres)
+	{
+		std::cout << sphere.GetPath() << std::endl;
+	}
+	
+
+	// -- Step 3
+	std::cout << "---- Step 3 ----" << std::endl;
+
+	/*! The Python commands in the interpreter
+		
+		primIter = iter(Usd.PrimRange.PreAndPostVisit(usdviewApi.stage.GetPseudoRoot()))
+		for x in primIter: print(x, primIter.IsPostVisit())
+
+		# Output:
+		Usd.Prim(</>) False
+		Usd.Prim(</refSphere>) False
+		Usd.Prim(</refSphere/world>) False
+		Usd.Prim(</refSphere/world>) True
+		Usd.Prim(</refSphere>) True
+		Usd.Prim(</refSphere2>) False
+		Usd.Prim(</refSphere2/world>) False
+		Usd.Prim(</refSphere2/world>) True
+		Usd.Prim(</refSphere2>) True
+		Usd.Prim(</>) True
+	*/
+
+	// Write the C++ equivalent of the python code above
+	pxr::UsdPrimRange pseudoRoot = pxr::UsdPrimRange::PreAndPostVisit(refStage->GetPseudoRoot());
+	std::cout << "All prims in the stage of RefExample.usda with pre and post visit:\n" << std::endl;
+	for (auto it = pseudoRoot.begin(); it != pseudoRoot.end(); ++it)
+	{
+		std::cout << (*it).GetPath() << " " << (it.IsPostVisit() ? "True" : "False") << std::endl;
+	}
+
+	// -- Step 4
+	std::cout << "---- Step 4 ----" << std::endl;
+
+	/*! The Python commands in the interpreter
+		
+		# First, manually inactivate the reference to refSphere2
+
+		# Then Print the prims in the stage
+		[x for x in usdviewApi.stage.Traverse()]
+		# Output: [Usd.Prim(</refSphere>), Usd.Prim(</refSphere/world>)]
+
+		[x for x in usdviewApi.stage.TraverseAll()]
+		# Output: [Usd.Prim(</refSphere>), Usd.Prim(</refSphere/world>), Usd.Prim(</refSphere2>)]
+	*/
+
+	// Write the C++ equivalent of the python code above
+	refStage->OverridePrim(pxr::SdfPath("/refSphere2")).SetActive(false);
+
+	//print the stage to check that it's marked as inactive
+	std::string fileResult;
+	refStage->GetRootLayer()->ExportToString(&fileResult);
+	std::cout << "Content of file RefExample.usda after deactivating refSphere2:\n" << fileResult << std::endl;
+
+	//Accoding to documentation: "refStage->Traverse(): Traverse the active, loaded, defined, non-abstract prims on this stage depth-first."
+	std::cout << "Prims (active, loaded, defined, non-abstract) in the stage of RefExample.usda after deactivating refSphere2:" << std::endl;
+	for (pxr::UsdPrim prim : refStage->Traverse())
+	{
+		std::cout << prim.GetPath() << std::endl;
+	}
+
+	std::cout << std::endl; //just for layout of the ouput
+
+	//According to documentation "refStage->TraverseAll(): Traverse all the prims on this stage depth-first."
+	std::cout << "All Prims in the stage of RefExample.usda after deactivating refSphere2:" << std::endl;
+	for (pxr::UsdPrim prim : refStage->TraverseAll())
+	{
+		std::cout << prim.GetPath() << std::endl;
+	}
+}
+
 
 int main()
 {
@@ -270,7 +392,9 @@ int main()
 
 	TestFunction_PixarTutorial_InspectAndAuthorProperties();
 
-	TestFunction_ReferencingLayers();
+	TestFunction_PixarTutorial_ReferencingLayers();
+
+	TestFunction_StageTraversal();
 
 	std::cout << "End of main." << std::endl;
 
